a9d4fe1dd75c0ebcdd4bcd1540a18e4dff461285
Sophia <tozadorsophia@gmail.com>
2024-04-29 08:35:13 -0300

    Iniciando tutorial de Python

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 @app.route('/')
 
+@app.route("/", methods=("GET",))
 def index():
     return jsonify({"status": 200, "message": "API da Sophia Tozador"})
     
-@app.route("/aleatorios")
+@app.route("/aleatorios", methods=("GET",))
 def aleatorios():
     import random
     a = random.randint(49, 100)
@@ -15,6 +16,24 @@ def aleatorios():
 @app.route("/argumentos/<string:nome>")
 def argumentos(nome: str):
     return jsonify({"status": 200, "data": nome})
+
+@app.route("/argumentos", methods=("GET",))
+def arg_implicito():
+    return jsonify({"status": 200, "data": request.args["nome"]})
+
+@app.route("/idades", methods=("GET",))
+def idades():
+    from random_data import pessoas
+    import funcoes
+    num = funcoes.maior_de_50(pessoas)
+    return jsonify({"status": 200, "data": num})
+
+@app.route("/salarios", methods=("GET",))
+def salarios():
+    from random_data import pessoas
+    import funcoes
+    num = funcoes.mais_2000(pessoas)
+    return jsonify({"status": 200, "data": num})
 
 if __name__ == '__main__':
     app.run(debug=True)
