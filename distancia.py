e5ff13324f43ea062ddc6b56004b8255d6c7622a
Sophia <tozadorsophia@gmail.com>
2024-04-29 15:02:12 -0300

    v0.1

---
+++ distancia.py
@@ -0,0 +1,5 @@
+distancia = float(input("Digite a distância da viagem em km: "))
+velocidade = float(input("Digite a velocidade média em km/h: "))
+tempo = distancia / velocidade
+
+print(f"O tempo de viagem será de {tempo:2} horas.")
