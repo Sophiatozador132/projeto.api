7a0bdee758c5490e0346c8b08a0db874e671a3d4
Sophia <tozadorsophia@gmail.com>
2024-05-07 10:49:46 -0300

    rota index e main

--- app.py
+++ app.py
@@ -9,40 +9,5 @@ def index():
 from api import bp
 app.register_blueprint(bp)
 
-if __name_ == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0")
-
-@app.route("/", methods=("GET",))
-def index():
-    return jsonify({"status": 200, "message": "API da Sophia Tozador"})
-    
-@app.route("/aleatorios", methods=("GET",))
-def aleatorios():
-    import random
-    a = random.randint(49, 100)
-    return jsonify({"status": 200, "data": a})
-
-@app.route("/argumentos/<string:nome>")
-def argumentos(nome: str):
-    return jsonify({"status": 200, "data": nome})
-
-@app.route("/argumentos", methods=("GET",))
-def arg_implicito():
-    return jsonify({"status": 200, "data": request.args["nome"]})
-
-@app.route("/idades", methods=("GET",))
-def idades():
-    from random_data import pessoas
-    import funcoes
-    num = funcoes.maior_de_50(pessoas)
-    return jsonify({"status": 200, "data": num})
-
-@app.route("/salarios", methods=("GET",))
-def salarios():
-    from random_data import pessoas
-    import funcoes
-    num = funcoes.mais_2000(pessoas)
-    return jsonify({"status": 200, "data": num})
-
-if __name__ == '__main__':
-    app.run(debug=True)
